383ba9057c57aaf8a24c73912ebaea8d2667abfa
karan <karangarale123@gmail.com>
2026-06-02 08:35:32 +0530

    CourseTopic backend CRUD completed

---
+++ backend/services/courseTopic.service.js
@@ -0,0 +1,21 @@
+import { getAllCourseTopics, getCourseTopicById, createCourseTopic, updateCourseTopic, deleteCourseTopic } from '../repositories/courseTopic.repository.js';
+
+export const createCourseTopicService = async (payload) => {
+    return createCourseTopic(payload);
+};
+
+export const getAllCourseTopicsService = async () => {
+    return getAllCourseTopics();
+};
+
+export const getCourseTopicByIdService = async (id) => {
+    return getCourseTopicById(id);
+};
+
+export const updateCourseTopicService = async (id, payload) => {
+    return updateCourseTopic(id, payload);
+};
+
+export const deleteCourseTopicService = async (id) => {
+    return deleteCourseTopic(id);
+};
